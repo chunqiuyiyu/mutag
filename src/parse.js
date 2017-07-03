f4ca62c2dd8921bdc8eba821f777e83f2b0ec8c6
春秋一语 <1725651735@qq.com>
2017-07-03 17:49:58 +0800

    Update parse.js

--- src/parse.js
+++ src/parse.js
@@ -13,6 +13,21 @@ export function parsePRIV (data, obj){
       obj['AverageLevel'] = getHexStr(data);
       break;
 
+    case 28:
+      // WM/UniqueFileIdentifier
+      obj['WM/UniqueFileIdentifier'] = getIdentifiler(data.slice(24));
+      break;
+
+    case 31:
+      // WM/WMContentID
+      obj['WM/WMContentID'] = getId(data.slice(14));
+      break;
+
+    case 34:
+      // WM/WMCollectionID
+      obj['WM/WMCollectionID'] = getId(data.slice(17));
+      break;
+
     case 39:
       // WM/MediaClassPrimaryID
       data = data.slice(-16);
@@ -27,7 +42,14 @@ export function parsePRIV (data, obj){
       obj['WM/MediaClassSecondaryID'] = data;
       break;
 
+    case 44:
+      // WM/Provider
+      console.log(data.slice(11));
+      obj['WM/Provider'] = getStr(data.slice(11));
+      break;
+
     default:
+      obj[data.length] = getStr(data);
       break;
   }
 
@@ -66,4 +88,29 @@ function genGuidStr (guid) {
 function getHexStr(data) {
   data = data.slice(-4, -2);
   return data[0].toString(16) + data[1].toString(16);
+}
+
+function getIdentifiler(data) {
+  return data.reduce((prev, curr) => {
+    if (prev === 59) {
+      prev = ';'
+    }
+
+    return String(prev) + String(curr);
+  });
+}
+
+function getId(data) {
+  return data.reduce((prev, curr) => {
+    return String(prev) + String(curr);
+  });
+}
+
+function getStr(data) {
+  let tmp = '';
+  data.map((curr) => {
+    tmp += String.fromCharCode(curr).replace(String.fromCharCode(0), '');
+  });
+
+  return tmp;
 }
