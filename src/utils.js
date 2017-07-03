17af446ba68662b1001f3b0bd11f283e667acc35
春秋一语 <1725651735@qq.com>
2017-07-03 17:53:15 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -14,16 +14,16 @@ export function getTagData(data, encode) {
   let seek = 0;
   let tmp, tmpStr = '';
 
-  if (encode === 0) {
+  if (encode === 0 && data[0] !== 1) {
     while (seek < data.length) {
-      if (data[seek] < 127 ) {
+      if (data[seek] < 127) {
         tmpStr += String.fromCharCode(data[seek]);
-        seek ++;
+        seek++;
       } else {
         tmp = data.slice(seek, seek + 2);
         if (tmp.length == 1) {
           tmpStr += new TextDecoder('iso-8859-1').decode(tmp.buffer);
-          seek ++;
+          seek++;
         } else {
           tmp = new Uint16Array(tmp.buffer);
           tmpStr += new TextDecoder('gbk').decode(tmp.buffer);
@@ -35,12 +35,51 @@ export function getTagData(data, encode) {
 
   if (encode === 1 || (data[0] === 1 && encode === 0)) {
     // the utf-16 string begin with FF FE -> 255 254
-    data = data.slice(data.lastIndexOf(254) + 1);
-    data = new Uint16Array(data.buffer);
+    if (data.lastIndexOf(254) !== -1) {
+      data = data.slice(data.lastIndexOf(254) + 1);
+      data = new Uint16Array(data.buffer);
+    }
+
     tmpStr = getStr(0, data.length, data);
   }
 
-  // delete the meaningless char
-  tmpStr.replace(String.fromCharCode(0), '');
+  tmpStr = filterStr(tmpStr);
   return tmpStr;
+}
+
+export function getImgIndex(data) {
+  // PNG begin with 8950(137, 80) while JPG begin with FFD8(255, 216)
+  const start = [255, 216];
+  let format = 'jpeg';
+
+  if (getStr(15, 18, data) === 'PNG') {
+    start[0] = 137;
+    start[1] = 80;
+    format = 'png';
+  }
+
+  let i = 0;
+  while (i < data.length)
+  {
+    if (start[0] === data[i] && start[1] === data[i + 1])
+    {
+        break;
+    }
+    i++;
+  }
+
+  return {
+    i,
+    format
+  };
+}
+
+function filterStr(str) {
+  // delete the meaningless char
+  const tmp = ['\u0000', String.fromCharCode(255), '\f'];
+  for (let char of tmp) {
+    str = str.replace(char, '');
+  }
+
+  return str;
 }
