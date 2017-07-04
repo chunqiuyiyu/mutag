e7cf6cf33fa4fa4e4f9a00046840ef6e5fe4db1c
春秋一语 <1725651735@qq.com>
2017-07-04 16:53:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ import fetchFile from './fetch';
 
 // define the module API and version
 const mutag = Object.create(null);
-const version = '1.0.0';
+const version = '2.0.0';
 
 mutag.version = version;
 mutag.fetch = fetchFile;
 
-// export the module for browser or nodejs
-if (typeof window !== 'undefined') {
-  window.mutag = mutag;
-} 
-
-// export default mutag;
-export {fetchFile as fetch, version}
+// export the module for browser
+window.mutag = mutag;
