0a1915df9dafae9151463acdbc1cf666907f7677
春秋一语 <1725651735@qq.com>
2017-07-17 17:08:27 +0800

    update version to 2.0.2

--- src/browser/index.js
+++ src/browser/index.js
@@ -5,7 +5,7 @@ import fetchFile from './fetch';
 
 // define the module API and version
 const mutag = Object.create(null);
-const version = '2.0.0';
+const version = '2.0.2';
 
 mutag.version = version;
 mutag.fetch = fetchFile;
